08cb99cbb384d0ca5823cbe0df6be466cae7b98c
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-09-23 14:04:53 -0300

    Adicionando função para funcionar sem validação.

--- src/Contexts/ValidacoesCadastro.js
+++ src/Contexts/ValidacoesCadastro.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const validacoesCadastro = React.createContext();
+const validacoesCadastro = React.createContext({
+  cpf: semValidacoes,
+  senha: semValidacoes,
+  nome: semValidacoes,
+});
+
+function semValidacoes(dados) {
+  console.log(dados);
+  return { valido: true, texto: "" };
+}
 
 export default validacoesCadastro;
